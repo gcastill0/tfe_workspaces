1d558c26be5fea5f76d78faabcfbdf5bad63a629
gcastill0 <gilberto@hashicorp.com>
2019-07-31 10:16:31 -0400

    Make changes

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "tfe_organization" "tfe_org" {
+  name  = "gcastill0"
+  email = "gilberto@hashicorp.com"
+}
+
+resource "tfe_workspace" "test" {
+  name         = "${workspace_name}"
+  organization = "${tfe_organization.tfe_org.id}"
+}
+
+resource "tfe_variable" "credentials" {
+  key          = "AWS_SECRET_ACCESS_KEY"
+  value        = "my_value_name"
+  category     = "env"
+  workspace_id = "${tfe_workspace.tfe_org.id}"
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+# variable "workspace_repo" {
+#   description = "VCS repo in the format org_name/repo_name."
+# }
+#
+# variable "workspace_name" {
+#   description = "Desired name for workspace."
+# }
+
